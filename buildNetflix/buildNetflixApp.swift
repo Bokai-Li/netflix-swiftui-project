25bdbf0c54349810fa0c143cd152ad9a0c80e043
Bokai Li <Bokai247@gmail.com>
2021-05-02 16:40:19 -0400

    Initial Commit

---
+++ buildNetflix/buildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  buildNetflixApp.swift
+//  buildNetflix
+//
+//  Created by Bokai Li on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct buildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
