883ed4efe0ea905ad3a7e19fe6dfc37281df0c47
Bokai Li <Bokai247@gmail.com>
2021-05-12 16:57:36 -0400

    color extension, search bar zstack

---
+++ buildNetflix/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  buildNetflix
+//
+//  Created by Bokai Li on 5/12/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let graySearchBackground = Color("gray_search_background")
+    static let graySearchText = Color("gray_search_text")
+}
