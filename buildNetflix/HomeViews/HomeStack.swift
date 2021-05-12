96ff926bee01ff0788372f751a513d120dfca700
Bokai Li <Bokai247@gmail.com>
2021-05-11 23:08:21 -0400

    showing overlay menus for genre and movie type

--- buildNetflix/HomeViews/HomeStack.swift
+++ buildNetflix/HomeViews/HomeStack.swift
@@ -11,6 +11,7 @@ struct HomeStack: View {
     var vm: HomeViewModel
     
     var topRowSelection: HomeTopRow
+    var selectedGenre: HomeGenre
     
     @Binding var movieDetailToShow: Movie?
     
@@ -26,7 +27,7 @@ struct HomeStack: View {
                 
                 ScrollView(.horizontal, showsIndicators: false){
                     LazyHStack{
-                        ForEach(vm.getMovie(forCat: category, andHomeRow: topRowSelection)){ movie in
+                        ForEach(vm.getMovie(forCat: category, andHomeRow: topRowSelection, andGenre: selectedGenre)){ movie in
                             StandardHomeMovie(movie: movie)
                                 .frame(width: 100, height: 200)
                                 .padding(.horizontal,20)
@@ -47,7 +48,7 @@ struct HomeStack_Previews: PreviewProvider {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             ScrollView{
-                HomeStack(vm: HomeViewModel(), topRowSelection: .movies, movieDetailToShow: .constant(nil))
+                HomeStack(vm: HomeViewModel(), topRowSelection: .movies, selectedGenre: .AllGenres, movieDetailToShow: .constant(nil))
             }
             .foregroundColor(.white)
         }
